ce6cbd02dd03fb6c06b8f3eaa40877861b78da5d
Dave1415 <dawitgetaneh99@gmail.com>
2022-06-04 22:24:49 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,13 @@
 int main(void)
 {
 char low;
-char up;
 for (low = 'a'; low <='z'; low++)
-{
 putchar(low);
-}
 /* print uppercase letters */
-for (up = 'a'; up = 'z'; up++)
-{
+for (low = 'A'; low = 'Z'; low++)
 putchar(up);
-}
+{
 putchar('\n');
+}
 return(0);
 }
