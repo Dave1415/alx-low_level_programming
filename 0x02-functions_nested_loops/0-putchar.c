9c2dc75994a05b5152059045fce91a5cf208f591
Dave1415 <dawitgetaneh99@gmail.com>
2022-06-07 03:45:45 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Block Entry
+ *Discription: a program that prints _putchar, followed by a new line.
+ *Return: 0 if successful
+ */
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
