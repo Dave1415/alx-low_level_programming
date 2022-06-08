9e1f41215800f5a2ea9d59fc42616d47a0da60ae
Dave1415 <dawitgetaneh99@gmail.com>
2022-06-08 09:58:51 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,5 +2,26 @@
 /**
  *print_sign - prints the sign of a number
  *
+ *@n:  the int to print
+ *Return: 0 if successful
+ */
+int print_sign(int n)
+{
+if (n > 0);
+{
+_putchar('+');
+return (1);
+}
+if else (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 
 }
+
