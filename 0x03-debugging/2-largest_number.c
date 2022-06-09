2edfd932b2db86c886c5122107624c8bf9fbc333
Dave1415 <dawitgetaneh99@gmail.com>
2022-06-09 07:35:18 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,12 +13,12 @@ if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b >= a && b >= c)    
-{      
-largest = b;      
+else if (b >= a && b >= c)
+{ 
+largest = b;
 }
-else    
-{      
+else
+{
 largest = c;
 }
 return (largest);
