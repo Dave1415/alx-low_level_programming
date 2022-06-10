4c7d517acf0b543e9587f249bae4f5f1df0d3271
Dave1415 <dawitgetaneh99@gmail.com>
2022-06-10 08:05:31 -0700

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,8 @@ for (c = 0; c <= 10; c++)
 for (i = 0; i <= 14; i++)
 {
 if (i >= 10)
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 }
 _putchar('\n');
 }
