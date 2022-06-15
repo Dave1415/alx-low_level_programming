fc4eddb93a47125ad5275323a4ee882ec3f41a48
Dave1415 <dawitgetaneh99@gmail.com>
2022-06-14 19:40:43 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  *_strlen - print a string
- *@str: string
+ *@s: string
  *
  *Return: the string
  */
 int _strlen(char *str)
 {
-int i = 0;
-for (str[i] != '\0')
-_putchar(str[i]);
-i++;
-_putchar('\n');
+int i;
+for (i = 0; [i] != '\0'; i++)
+return (i);
 }
 
