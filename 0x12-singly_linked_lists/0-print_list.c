b1ccd40f3a6c92d39811a4b3c47dbd1666a02d4f
Dave1415 <dawitgetaneh99@gmail.com>
2022-07-15 09:05:31 -0700

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,19 +7,20 @@
  */
 size_t print_list(const list_t *h)
 {
-  int numb_node = 0;
-  while (h != '\0')
-    {
-      if (h->str != NULL)
-	{
-	  printf("[%d] %s\n", h->len, h->str);
-	}
-      else
-	{
-	  printf("[0] %p\n", h->str);
-	}
-      h = h->next;
-      numb_node++;
-    }
-  return (numb_node);
+unsigned int nodes = 0;
+while (h)
+{
+printf("[%u]", h->len);
+if (h->str == NULL)
+{
+printf("(nil)\n");
+}
+else
+{
+printf("%s\n", h->str);
+}
+h = h->next;
+node++;
+}
+return (node);
 }
